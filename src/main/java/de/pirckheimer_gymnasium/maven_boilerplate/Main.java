d8e6e36e18702497114818cb1b096f1eb7b2fe5c
Josef Friedrich <josef@friedrich.rocks>
2024-07-10 18:09:09 +0200

    Add first class

---
+++ src/main/java/de/pirckheimer_gymnasium/maven_boilerplate/Main.java
@@ -0,0 +1,11 @@
+package de.pirckheimer_gymnasium.maven_boilerplate;
+
+import de.pirckheimer_gymnasium.engine_pi.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game.start();
+    }
+
+}
